b8298618aa16bf34c4a565cffd0c5388e0586afb
Lhie Xhian <lhiexhian@gmail.com>
2025-07-03 14:05:25 +0800

    added a background music

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace Allonsy.Console;
 
 using System;
+using System.Media;
+using System.Threading;
 
 class Program
 {
@@ -14,6 +16,19 @@ class Program
     const int viewWidth = 5;
     const int viewHeight = 5;
     const int tileSize = 5;
+
+    // Ensure the System.Windows.Extensions NuGet package is installed in your project.
+    // You can install it using the NuGet Package Manager or the following command:
+    // dotnet add package System.Windows.Extensions
+    public static void PlayMusic(string filepath)
+    {
+        SoundPlayer musicPlayer = new SoundPlayer
+        {
+            SoundLocation = filepath
+        };
+        musicPlayer.Load(); // Ensure the file is loaded before playing
+        musicPlayer.Play();
+    }
 
     static void InitializeTiles()
     {
@@ -29,12 +44,14 @@ class Program
                 if (isGrass)
                 {
                     grass[i, j] = ".";
-                } else
+                }
+                else
                 {
                     grass[i, j] = ",";
                 }
             }
-        };
+        }
+        ;
         tiles["grass"] = grass;
 
         string[,] water = new string[tileSize, tileSize];
@@ -52,7 +69,7 @@ class Program
         {
             for (int j = 0; j < tileSize; j++)
             {
-                    wallV[i, j] = "▓";
+                wallV[i, j] = "▓";
             }
         }
         tiles["wallV"] = wallV;
@@ -62,13 +79,6 @@ class Program
         {
             for (int j = 0; j < tileSize; j++)
             {
-                //if (i == 4)
-                //{
-                //    tiles["wallH"][i, j] = "▒";
-                //} else
-                //{
-                //    tiles["wallH"][i, j] = "▓";
-                //}
                 wallH[i, j] = (i >= 3) ? "▒" : "▓";
             }
         }
@@ -79,7 +89,7 @@ class Program
         {
             for (int j = 0; j < tileSize; j++)
             {
-                player[i, j] = grass[i,j];
+                player[i, j] = grass[i, j];
             }
         }
         player[2, 2] = "O";
@@ -127,7 +137,8 @@ class Program
             playerY += addY;
             prevTile = map2D[playerY, playerX]; // Store current tile type
             map2D[playerY, playerX] = "player"; // Set player position
-        } else
+        }
+        else
         {
             return; // Prevent moving into walls or out of bounds
         }
@@ -179,14 +190,9 @@ class Program
         map2D[playerY, playerX] = "player"; // Set player position
     }
 
-
-
-
-
-
-
     static void Main(string[] args)
     {
+        PlayMusic("bgm.wav");
         Console.CursorVisible = false;
         System.Console.Title = "Wisinshu";
         System.Console.SetWindowSize(55, 42);
@@ -221,254 +227,3 @@ class Program
         }
     }
 }
-
-
-
-
-
-//namespace Allonsy.Console
-//{
-//    //class Player
-//    //{
-//    //    private string _uName;
-
-//    //    public string Name
-//    //    {
-//    //        get { return _uName; }
-//    //        set { _uName = value; }
-//    //    }
-
-//    //    public void GetName(string text)
-//    //    {
-//    //        System.Console.WriteLine(text + _uName);
-//    //    }
-//    //    public string GetName()
-//    //    {
-//    //        return _uName;
-//    //    }
-
-//    //    public void SetName(string Name)
-//    //    {
-//    //        _uName = Name;
-//    //    }
-//    //}
-
-
-
-//    class Program
-//    {
-//        static char[,] map2D;
-//        static Dictionary<char, string[,]> tileItem;
-//        static int tileWidth = 5;
-//        static int tileHeight = 5;
-//        static int playerX = 1;
-//        static int playerY = 1;
-//        static Random rand = new Random();
-
-//        static void TileSet()
-//        {
-//            tileItem = new Dictionary<char, string[,]>();
-
-//            string[,] grass = new string[5, 5];
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    bool isGrass = rand.Next(0, 2) == 0;
-//                    if (isGrass)
-//                    {
-//                        grass[i, j] = ".";
-//                    }
-//                    else
-//                    {
-//                        grass[i, j] = ",";
-//                    }
-//                }
-//            }
-//            tileItem['.'] = grass;
-
-//            string[,] water = new string[5, 5];
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    water[i, j] = "~";
-//                }
-//            }
-//            water[0, 0] = " ";
-//            tileItem['~'] = water;
-
-//            string[,] wall = new string[5, 5];
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    if (i == 4)
-//                    {
-//                        wall[i, j] = "▒";
-//                    }
-//                    else if (j == 0 || j == 4 || i == 0 || i == 3)
-//                    {
-//                        wall[i, j] = "▓";
-//                    }
-//                    else
-//                    {
-//                        wall[i, j] = " ";
-//                    }
-//                }
-//            }
-//            tileItem['w'] = wall;
-
-//            string[,] wall2 = new string[5, 5];
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    wall2[i, j] = (j == 0 || j == 4 || i == 0 || i == 4) ? "▓" : " ";
-//                }
-//            }
-//            tileItem['W'] = wall2;
-
-//            string[,] player = new string[5, 5];
-//            for (int i = 0; i < 5; i++)
-//            {
-//                for (int j = 0; j < 5; j++)
-//                {
-//                    player[i, j] = grass[i, j];
-//                }
-//            }
-//            player[2, 2] = "O";
-//            player[3, 1] = "/";
-//            player[3, 2] = "T";
-//            player[3, 3] = "\\";
-//            player[4, 2] = "A";
-//            tileItem['p'] = player;
-//        }
-
-
-    //    static void detectCollision(int addX, int addY)
-    //{
-    //    playerX += addX;
-    //    playerY += addY;
-
-
-    //    if (map2D[playerY, playerX] == '.')
-    //    {
-    //        map2D[playerY, playerX] = 'p';
-    //    }
-    //}
-
-//        static void LogicMap()
-//        {
-//            int width = 10;
-//            int height = 8;
-//            map2D = new char[height, width];
-
-//            int x, y;
-//            x = 0;
-//            for (y = 0; y < height; y++)
-//            {
-//                for (x = 0; x < width; x++)
-//                {
-//                    if (y == 0 && x != 0 && x != width-1 || y == height - 1 )
-//                    {
-//                        map2D[y, x] = 'w';
-//                    }
-//                    else if (x == 0 || x == width - 1)
-//                    {
-//                        map2D[y, x] = 'W';
-//                    }
-//                    else
-//                    {
-//                        map2D[y, x] = '.';
-//                    }
-//                }
-//            }
-//        }
-
-//        static void DrawMap()
-//        {
-//            //System.Console.Clear();
-//            System.Console.SetCursorPosition(0, 0);
-//            for (int y = 0; y < map2D.GetLength(0); y++)
-//            {
-//                for (int row = 0; row < tileHeight; row++)
-//                {
-//                    for (int x = 0; x < map2D.GetLength(1); x++)
-//                    {
-//                        char tileId = map2D[y, x];
-//                        string[,] tile = tileItem[tileId];
-
-//                        for (int col = 0; col < tileWidth; col++)
-//                        {
-//                            switch (tile[row, col])
-//                            {
-//                                case "▓":
-//                                case "▒":
-//                                    System.Console.ForegroundColor = ConsoleColor.DarkGray;
-//                                    break;
-
-//                                case "~":
-//                                    System.Console.ForegroundColor = ConsoleColor.Blue;
-//                                    break;
-
-//                                case ".":
-//                                    System.Console.ForegroundColor = ConsoleColor.DarkGreen;
-//                                    break;
-//                                case ",":
-//                                    System.Console.ForegroundColor = ConsoleColor.Green;
-//                                    break;
-
-//                                default:
-//                                    System.Console.ForegroundColor = ConsoleColor.White;
-//                                    break;
-//                            }
-//                            System.Console.Write(tile[row, col]);
-//                        }
-//                    }
-//                    System.Console.WriteLine();
-//                }
-//            }
-//            map2D[playerY, playerX] = 'p'; // Set player position
-//        }
-
-//        static void Main(string[] args)
-//        {
-//            System.Console.Title = "Wisinshu";
-//            System.Console.SetWindowSize(55, 42);
-//            System.Console.ForegroundColor = ConsoleColor.White;
-
-//            TileSet();
-//            LogicMap();
-//    DrawMap();
-//            while (true)
-//            {
-//                DrawMap();
-//var key = System.Console.ReadKey().Key;
-//                switch (key)
-//                {
-//                    case ConsoleKey.W:
-//                    case ConsoleKey.UpArrow:
-//                        detectCollision(0, -1); // Move up
-//                        break;
-//                    case ConsoleKey.S:
-//                    case ConsoleKey.DownArrow:
-//                        detectCollision(0, 1); // Move down
-//                        break;
-//                    case ConsoleKey.A:
-//                    case ConsoleKey.LeftArrow:
-//                        detectCollision(-1, 0); // Move left
-//                        break;
-//                    case ConsoleKey.D:
-//                    case ConsoleKey.RightArrow:
-//                        detectCollision(1, 0); // Move right
-//                        break;
-//                }
-//            }
-//            Thread.Sleep(50);
-//            //var player = new Player();
-//            //player.SetName(System.Console.ReadLine());
-//            //player.GetName("Player Name: ");
-//        }
-//    }
-//}
